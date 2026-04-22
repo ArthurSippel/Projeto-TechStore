c57d663c6ef506514b6523ae758360acd3c0de64
ArthurSippel <zicosippel@gmail.com>
2026-04-22 00:14:26 -0300

    commit 3
    apenas fazer estilo no css
    e revisar lógica CRUD para estudo

--- Aula 9 - Projeto TECHSTORE - (13.04.2026)/index.js
+++ Aula 9 - Projeto TECHSTORE - (13.04.2026)/index.js
@@ -87,7 +87,7 @@ app.get('/clientes/editar/:id', (req, res) => {
     );
 });
 
-//rota para editar dados
+//rota para editar dados form -> banco
 app.post('/clientes/atualizar', (req, res) => {
 
     const { id_clientes, nome_clientes, email_clientes, telefone_clientes } = req.body;
@@ -134,39 +134,241 @@ app.get('/clientes/deletar/:id', (req, res) => {
 // PRODUTOS
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
+//rota para a pág produtos
+app.get('/produtos', (req, res) => {
+
+    const query = ' SELECT produtos.*, categorias.nome_categorias FROM produtos INNER JOIN categorias ON produtos.id_categorias = categorias.id_categorias';
+
+    conectar.query(query, (err, results) => {
+
+        if (err) {
+            console.error(err);
+            return res.send('Erro ao buscar produtos');
+        }
+
+        res.render('produtos', { registros: results });
+    });
+
+});
+
+
+//rota para pág do form de produtos
+app.get('/produtos/form', (req, res) => {
+
+    conectar.query('SELECT * FROM categorias', (err, categorias) => {
+
+        res.render('form', {
+            tipo: 'produto',
+            categorias
+        });
+
+    });
+
+});
+
+
+// rota para os dados passarem form -> banco
+app.post('/produtos/adicionar', (req, res) => {
+
+    const { nome_produtos, preco_produtos, id_categorias } = req.body;
+
+    conectar.query(
+        'INSERT INTO produtos (nome_produtos, preco_produtos, id_categorias) VALUES (?, ?, ?)',
+        [nome_produtos, preco_produtos, id_categorias],
+        (err) => {
+
+            if (err) {
+                console.error(err);
+                return res.send('Erro ao cadastrar produto');
+            }
+
+            res.redirect('/produtos');
+        }
+    );
+});
+
+
+//rota para a pág de edição
+app.get('/produtos/editar/:id', (req, res) => {
+
+    conectar.query(
+        'SELECT * FROM produtos WHERE id_produtos = ?',
+        [req.params.id],
+        (err, results) => {
+
+            if (err) {
+                console.error(err);
+                return res.send('Erro ao buscar produto');
+            }
+
+            conectar.query('SELECT * FROM categorias', (err, categorias) => {
+
+                res.render('editar', {
+                    tipo: 'produto',
+                    registro: results[0],
+                    categorias
+                });
+            });
+        }
+    );
+});
+
+
+//rota para editar dados form -> banco
+app.post('/produtos/atualizar', (req, res) => {
+
+    const { id_produtos, nome_produtos, preco_produtos, id_categorias } = req.body;
+
+    conectar.query(
+        'UPDATE produtos SET nome_produtos=?, preco_produtos=?, id_categorias=? WHERE id_produtos=?',
+        [nome_produtos, preco_produtos, id_categorias, id_produtos],
+        (err) => {
+
+            if (err) {
+                console.error(err);
+                return res.send('Erro ao atualizar');
+            }
+
+            res.redirect('/produtos');
+        }
+    );
+});
+
+//rota deletar produto
+app.get('/produtos/deletar/:id', (req, res) => {
+
+    conectar.query(
+        'DELETE FROM produtos WHERE id_produtos=?',
+        [req.params.id],
+        (err) => {
+
+            if (err) {
+                console.error(err);
+                return res.send('Erro ao deletar');
+            }
+
+            res.redirect('/produtos');
+        }
+    );
+});
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// CATEGORIAS
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+//rota para pág da categoria
+app.get('/categorias', (req, res) => {
+
+    conectar.query('SELECT * FROM categorias', (err, results) => {
+
+        if (err) {
+            console.error(err);
+            return res.send('Erro ao buscar categorias');
+        }
+
+        res.render('categorias', { registros: results });
+    });
+
+});
+
+
+//rota para pág do form de categorias
+app.get('/categorias/form', (req, res) => {
+    res.render('form', { tipo: 'categoria' });
+});
+
+
+//rota rota para os dados passarem form -> banco
+app.post('/categorias/adicionar', (req, res) => {
+
+    const { nome_categorias } = req.body;
+
+    conectar.query(
+        'INSERT INTO categorias (nome_categorias) VALUES (?)',
+        [nome_categorias],
+        (err) => {
+
+            if (err) {
+                console.error(err);
+                return res.send('Erro ao cadastrar');
+            }
+
+            res.redirect('/categorias');
+        }
+    );
+});
+
+
+//rota para a pág de edição
+app.get('/categorias/editar/:id', (req, res) => {
+
+    conectar.query(
+        'SELECT * FROM categorias WHERE id_categorias = (?)',
+        [req.params.id],
+        (err, results) => {
+
+            if (err) {
+                console.error(err);
+                return res.send('Erro ao buscar categoria');
+            }
+
+            res.render('editar', {
+                tipo: 'categoria',
+                registro: results[0]
+            });
+        }
+    );
+});
+
+
+//rota para editar dados form -> banco
+app.post('/categorias/atualizar', (req, res) => {
+
+    const { id_categorias, nome_categorias } = req.body;
+
+    conectar.query(
+        'UPDATE categorias SET nome_categorias=? WHERE id_categorias=?',
+        [ nome_categorias, id_categorias ],
+        (err) => {
+
+            if (err) {
+                console.error(err);
+                return res.send('Erro ao atualizar');
+            }
+
+            res.redirect('/categorias');
+        }
+    );
+});
+
+//rota deletar produto
+app.get('/categorias/deletar/:id', (req, res) => {
+
+    conectar.query(
+        'DELETE FROM categorias WHERE id_categorias=?',
+        [req.params.id],
+        (err) => {
+
+            if (err) {
+                console.error(err);
+                return res.send('Erro ao deletar');
+            }
+
+            res.redirect('/categorias');
+        }
+    );
+});
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
 
